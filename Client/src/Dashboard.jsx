389da61fe89116763b94233888bf291202cf1dfc
Harshvardhan More <harshmore947@gmail.com>
2025-02-05 20:36:33 +0530

    Fix env import issue

--- Client/src/Dashboard.jsx
+++ Client/src/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-require('dotenv').config();
 
 const Dashboard = ({ user }) => {
   const [profile, setProfile] = useState({
